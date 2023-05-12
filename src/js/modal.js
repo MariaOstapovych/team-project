b041dbebb9005f8a899cf10dcbf9552909b894e2
Dzhaman-Nazarii <dzhaman.nazar2003@gmail.com>
2023-05-12 19:55:38 +0300

    розмітка модального вікна

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,1 +1,9 @@
 // Nazar
+const btnOpen = document.querySelector('.modal__open');
+
+btnOpen.addEventListener('click', onClick);
+function onClick(evt) {
+  evt.preventDefault();
+  console.log('click');
+  btnOpen.classList.remove('visibility');
+}
