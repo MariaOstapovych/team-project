ee48e1271eb0a9ffc67855782d9c2f76a810c481
Dzhaman-Nazarii <dzhaman.nazar2003@gmail.com>
2023-05-13 11:08:24 +0300

    закриття модалки

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,9 +1,34 @@
 // Nazar
-const btnOpen = document.querySelector('.modal__open');
+const btnModalOpen = document.querySelector('.modal__open');
+const btnModalCLose = document.querySelector('.modal__close-btn');
+const backdrop = document.querySelector('.backdrop');
+const modal = document.querySelector('.modal');
 
-btnOpen.addEventListener('click', onClick);
+btnModalOpen.addEventListener('click', onClick);
+
 function onClick(evt) {
   evt.preventDefault();
-  console.log('click');
-  btnOpen.classList.remove('visibility');
+  backdrop.style.display = 'block';
+}
+
+document.addEventListener('keydown', onEsc);
+function onEsc(evt) {
+  evt.preventDefault();
+  if (evt.key === 'Escape') {
+    backdrop.style.display = 'none';
+  }
+}
+
+backdrop.addEventListener('click', onBackdropClick);
+function onBackdropClick(evt) {
+  if (evt.target === backdrop) {
+    backdrop.style.display = 'none';
+  }
+}
+
+btnModalCLose.addEventListener('click', onBtnClose);
+function onBtnClose(evt) {
+  evt.preventDefault();
+  evt.preventDefault();
+  backdrop.style.display = 'none';
 }
