1a5417a1d54204e3e379bf16efcbdf0f9ef6f6dc
Andrii Stepanenko <andrii.stp@gmail.com>
2023-05-14 15:38:16 +0300

    button + lorem10500 test

--- src/js/scroll-up.js
+++ src/js/scroll-up.js
@@ -1,1 +1,20 @@
 const scrollUp = document.querySelector('.scroll-up');
+
+scrollUp.addEventListener('click'.onScrolClick);
+
+function onScrolClick() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+}
+
+window.addEventListener('scroll', onScrollWheel);
+
+function onScrollWheel() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollUp.style.display = 'block';
+  } else {
+    backToTopButton.style.display = 'none';
+  }
+}
