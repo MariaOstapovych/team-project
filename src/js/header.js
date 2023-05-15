b2a052a6c5eccab18f0fb97fb0d42312926ee637
IhorChepak <chepak_lex@ukr.net>
2023-05-15 19:07:47 +0300

    header_01

--- src/js/header.js
+++ src/js/header.js
@@ -1,1 +1,86 @@
 // Igor
+// Igor
+
+import openCloseIcon from '../images/icons.svg';
+
+// ВІДПРИТТЯ/ЗАКРИТТЯ КНОПОК АВТОРИЗАЦІЇ
+
+const userBtnEl = document.querySelector('.user-btn-js');
+const logOutBtnEl = document.querySelector('.log-out-btn-js');
+const logInBtnEl = document.querySelector('.drop-auth__modal-open-js');
+
+const handleUserBtnClick = () => {
+    logOutBtnEl.classList.toggle('is-hidden');
+};
+
+userBtnEl.addEventListener('click', handleUserBtnClick);
+logInBtnEl.addEventListener('click', toggleModal);
+
+// ВИДІЛЕННЯ ЖОВТИМ НАЗВУ ПОТОЧНОЇ СТОРІНКИ (меню в хедері)
+const menuHomeEl = document.querySelector('.menu__home');
+const menuShoppingEl = document.querySelector('.menu__shopping');
+
+const setCurrentPage = () => {
+    const currentPageName = window.location.pathname;
+    // console.log(currentPageName);
+    // console.log(menuHomeEl);
+    // console.log(menuShoppingEl);
+
+    if (
+        currentPageName === '/index.html' ||
+        currentPageName === '/books-project/index.html' ||
+        currentPageName === '/books-project/'
+    ) {
+        menuHomeEl.classList.add('current');
+        menuShoppingEl.classList.remove('current');
+        return;
+    }
+    menuShoppingEl.classList.add('current');
+    menuHomeEl.classList.remove('current');
+};
+
+setCurrentPage();
+
+//   ВІДКРИТТЯ/ЗАКРИТТЯ МОДАЛКИ ДЛЯ МОБ. ВЕРСІЇ
+
+const modalBoxEl = document.querySelector('.data-modal');
+const openModalBtnEl = document.querySelector('.js-open-menu');
+const iconHrefEl = document.querySelector('.icon-href');
+
+// const mobbEl = document.querySelector('.mob-header');
+
+openModalBtnEl.addEventListener('click', toggleModal);
+
+function toggleModal() {
+    if (modalBoxEl.classList.contains('is-hidden')) {
+        modalBoxEl.classList.remove('is-hidden');
+        iconHrefEl.setAttribute('href', `${openCloseIcon}#icon-X-close`);
+        document.body.classList.add('modal-open');
+
+        return;
+    } else {
+        modalBoxEl.classList.add('is-hidden');
+        iconHrefEl.setAttribute('href', `${openCloseIcon}#icon-burger`);
+        document.body.classList.remove('modal-open');
+    }
+}
+
+// ВИДІЛЕННЯ ЖОВТИМ, НАЗВУ ПОТОЧНОЇ СТОРІНКИ (меню в МОДАЛЦІ)
+
+const dropMenuHomeEl = document.querySelector('.drop-menu__home');
+const dropMenuShoppingEl = document.querySelector('.drop-menu__shopping');
+
+const dropSetCurrentPage = () => {
+    const dropCurrentPageName = window.location.pathname;
+
+    if (
+        dropCurrentPageName === '/index.html' ||
+        dropCurrentPageName === '/team-project/index.html'
+    ) {
+        dropMenuHomeEl.classList.add('current');
+        return;
+    }
+    dropMenuShoppingEl.classList.add('current');
+};
+
+dropSetCurrentPage();
