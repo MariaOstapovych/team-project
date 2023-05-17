6e7be25fb5b4443f43a0cfd00351efb128285c30
Symeon Melnychuk <m.simeon998@gmail.com>
2023-05-17 21:47:07 +0300

    home-page(15)

--- src/js/home-page.js
+++ src/js/home-page.js
@@ -19,13 +19,8 @@ function createDate(categ) {
     data.forEach(book => {
       bookMarkup += `<li id="${book._id}"><img class="best-books__image" src="${book.book_image}"alt="${book.title}"><p>${book.title}</p><p>${book.author}</p></li>`;
     });
-    markup += `
-           <li>
-           <p>${arr.list_name}</p>
-            <ul>${bookMarkup}</ul>
-           <button type="button" data-category="${arr.list_name}">see more
-           </button>
-           </li>`;
+    markup += `<li><p>${arr.list_name}</p><ul>${bookMarkup}</ul><button type="button" data-category="${arr.list_name}">see more
+</button></li>`;
   });
   
   bestBooks.insertAdjacentHTML('afterbegin', markup);
