42f860dc276bf9fa15a9207e063794131805cb14
MariaOstapovych <m.ost.olgv@gmail.com>
2023-05-17 21:46:35 +0300

    fix home-page

--- src/js/home-page.js
+++ src/js/home-page.js
@@ -13,21 +13,19 @@ promise.then(data => createDate(data));
 function createDate(categ) {
   let markup = '';
 
-  categ.map(arr => {
+  categ.forEach(arr => {
     let bookMarkup = '';
     const data = arr.books;
     data.forEach(book => {
-      bookMarkup += <li id="${book._id}"><img class="best-books__image" src="${book.book_image}"alt="${book.title}"><p>${book.title}</p><p>${book.author}</p></li>;
+      bookMarkup += `<li id="${book._id}"><img class="best-books__image" src="${book.book_image}" alt="${book.title}"><p>${book.title}</p><p>${book.author}</p></li>`;
     });
     markup += `
-           <li>
-           <p>${arr.list_name}</p>
-            <ul>${bookMarkup}</ul>
-           <button type="button" data-category="${arr.list_name}">see more
-           </button>
-           </li>`;
+      <li>
+        <p>${arr.list_name}</p>
+        <ul>${bookMarkup}</ul>
+        <button type="button" data-category="${arr.list_name}">see more</button>
+      </li>`;
   });
   
   bestBooks.insertAdjacentHTML('afterbegin', markup);
 }
-
