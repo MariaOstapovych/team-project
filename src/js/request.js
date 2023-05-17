157081287247f6e97a6c1918661425b654a0d06d
MariaOstapovych <m.ost.olgv@gmail.com>
2023-05-18 02:24:39 +0300

    add fix

--- src/js/request.js
+++ src/js/request.js
@@ -22,7 +22,7 @@ export async function getTopBooks() {
 
 export async function getBooksCategory(selectedCategory) {
   const booksCategory = await axios.get(
-    'https://books-backend.p.goit.global/books/category?category=${selectedCategory}'
+    https://books-backend.p.goit.global/books/category?category=${selectedCategory}
   );
 
   return booksCategory.data;
@@ -36,7 +36,7 @@ export async function getBooksCategory(selectedCategory) {
 
 export async function getBookId(bookId) {
   const bookById = await axios.get(
-    'https://books-backend.p.goit.global/books/${bookId}'
+    https://books-backend.p.goit.global/books/${bookId}
   );
   return bookById.data;
 }
