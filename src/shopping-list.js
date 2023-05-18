d4044798110ef6bd45b08991bbd4f7766dca4455
Symeon Melnychuk <m.simeon998@gmail.com>
2023-05-18 09:50:30 +0300

    bag fix

--- src/shopping-list.js
+++ src/shopping-list.js
@@ -11,142 +11,17 @@ import amazonPNG from '../src/images/shopping-list/amazon@2x.png';
 import ibooksPNG from '../src/images/shopping-list/books@2x.png';
 import bookshopPNG from '../src/images/shopping-list/bookshop@2x.png';
 
-const data = [
-  {
-    _id: '642fd89ac8cf5ee957f12361',
-    list_name: 'Middle Grade Paperback Monthly',
-    date: '2023-04-07T08:46:57.000Z',
-    age_group: '',
-    amazon_product_url:
-      'https://www.amazon.com/Wish-Barbara-OConnor/dp/1250144051?tag=NYTBSREV-20',
-    article_chapter_link: '',
-    author: "Barbara O'Connor",
-    book_image:
-      'https://storage.googleapis.com/du-prd/books/images/9781250144058.jpg',
-    book_image_width: 330,
-    book_image_height: 485,
-    book_review_link: '',
-    book_uri: 'nyt://book/46604242-8624-57d1-bdd4-424c21cde273',
-    contributor: "by Barbara O'Connor",
-    contributor_note: '',
-    created_date: '2023-04-05 23:10:17',
-    description:
-      'There’s a well-known story of an expert, who was called by a company to look at their manufacturing system. The system had broken down, and everything was at a standstill. When the expert arrived, he carried nothing but a little black bag. Silently he walked around the equipment for a few minutes and then stopped. As he focused on one specific area of the equipment, he pulled out a small hammer from his bag and tapped it gently. Suddenly everything began to run again, and he quietly left the factory. The next day he sent a bill, that made the manager go ballistic.',
-    first_chapter_link: '',
-    price: '0.00',
-    primary_isbn10: '1250144051',
-    primary_isbn13: '9781250144058',
-    publisher: 'Square Fish',
-    rank: 1,
-    rank_last_week: 0,
-    sunday_review_link: '',
-    title: 'WISH',
-    updated_date: '2023-04-05 23:10:17',
-    weeks_on_list: 0,
-    buy_links: [
-      {
-        name: 'Amazon',
-        url: 'https://www.amazon.com/Wish-Barbara-OConnor/dp/1250144051?tag=NYTBSREV-20',
-      },
-      {
-        name: 'Apple Books',
-        url: 'https://goto.applebooks.apple/9781250144058?at=10lIEQ',
-      },
-      {
-        name: 'Barnes and Noble',
-        url: 'https://www.anrdoezrs.net/click-7990613-11819508?url=https%3A%2F%2Fwww.barnesandnoble.com%2Fw%2F%3Fean%3D9781250144058',
-      },
-      {
-        name: 'Books-A-Million',
-        url: 'https://du-gae-books-dot-nyt-du-prd.appspot.com/redirect?url1=https%3A%2F%2Fwww.anrdoezrs.net%2Fclick-7990613-35140%3Furl%3Dhttps%253A%252F%252Fwww.booksamillion.com%252Fp%252FWISH%252FBarbara%252BO%252527Connor%252F9781250144058&url2=https%3A%2F%2Fwww.anrdoezrs.net%2Fclick-7990613-35140%3Furl%3Dhttps%253A%252F%252Fwww.booksamillion.com%252Fsearch%253Fquery%253DWISH%252BBarbara%252BO%252527Connor',
-      },
-      {
-        name: 'Bookshop',
-        url: 'https://du-gae-books-dot-nyt-du-prd.appspot.com/redirect?url1=https%3A%2F%2Fbookshop.org%2Fa%2F3546%2F9781250144058&url2=https%3A%2F%2Fbookshop.org%2Fbooks%3Faffiliate%3D3546%26keywords%3DWISH',
-      },
-      {
-        name: 'IndieBound',
-        url: 'https://du-gae-books-dot-nyt-du-prd.appspot.com/redirect?url1=https%3A%2F%2Fwww.indiebound.org%2Fbook%2F9781250144058%3Faff%3DNYT&url2=https%3A%2F%2Fwww.indiebound.org%2Fsearch%2Fbook%3Fkeys%3DWISH%2BBarbara%2BO%2527Connor%26aff%3DNYT',
-      },
-    ],
-    __v: 0,
-  },
 
-  {
-    _id: '642fd89ac8cf5ee957f00000',
-    list_name: 'FOCUSIMA',
-    date: '2023-04-07T08:46:57.000Z',
-    age_group: '',
-    amazon_product_url:
-      'https://www.amazon.com/Wish-Barbara-OConnor/dp/1250144051?tag=NYTBSREV-20',
-    article_chapter_link: '',
-    author: 'Symeon Melnychuk',
-    book_image:
-      'https://storage.googleapis.com/du-prd/books/images/9781250144058.jpg',
-    book_image_width: 330,
-    book_image_height: 485,
-    book_review_link: '',
-    book_uri: 'nyt://book/46604242-8624-57d1-bdd4-424c21cde273',
-    contributor: "by Barbara O'Connor",
-    contributor_note: '',
-    created_date: '2023-04-05 23:10:17',
-    description:
-      'There’s a well-known story of an expert, who was called by a company to look at their manufacturing system. The system had broken down, and everything was at a standstill. When the expert arrived, he carried nothing but a little black bag. Silently he walked around the equipment for a few minutes and then stopped. As he focused on one specific area of the equipment, he pulled out a small hammer from his bag and tapped it gently. Suddenly everything began to run again, and he quietly left the factory. The next day he sent a bill, that made the manager go ballistic.',
-    first_chapter_link: '',
-    price: '0.00',
-    primary_isbn10: '1250144051',
-    primary_isbn13: '9781250144058',
-    publisher: 'Square Fish',
-    rank: 1,
-    rank_last_week: 0,
-    sunday_review_link: '',
-    title: 'FOCUSIMA',
-    updated_date: '2023-04-05 23:10:17',
-    weeks_on_list: 0,
-    buy_links: [
-      {
-        name: 'Amazon',
-        url: 'https://www.amazon.com/Wish-Barbara-OConnor/dp/1250144051?tag=NYTBSREV-20',
-      },
-      {
-        name: 'Apple Books',
-        url: 'https://goto.applebooks.apple/9781250144058?at=10lIEQ',
-      },
-      {
-        name: 'Barnes and Noble',
-        url: 'https://www.anrdoezrs.net/click-7990613-11819508?url=https%3A%2F%2Fwww.barnesandnoble.com%2Fw%2F%3Fean%3D9781250144058',
-      },
-      {
-        name: 'Books-A-Million',
-        url: 'https://du-gae-books-dot-nyt-du-prd.appspot.com/redirect?url1=https%3A%2F%2Fwww.anrdoezrs.net%2Fclick-7990613-35140%3Furl%3Dhttps%253A%252F%252Fwww.booksamillion.com%252Fp%252FWISH%252FBarbara%252BO%252527Connor%252F9781250144058&url2=https%3A%2F%2Fwww.anrdoezrs.net%2Fclick-7990613-35140%3Furl%3Dhttps%253A%252F%252Fwww.booksamillion.com%252Fsearch%253Fquery%253DWISH%252BBarbara%252BO%252527Connor',
-      },
-      {
-        name: 'Bookshop',
-        url: 'https://du-gae-books-dot-nyt-du-prd.appspot.com/redirect?url1=https%3A%2F%2Fbookshop.org%2Fa%2F3546%2F9781250144058&url2=https%3A%2F%2Fbookshop.org%2Fbooks%3Faffiliate%3D3546%26keywords%3DWISH',
-      },
-      {
-        name: 'IndieBound',
-        url: 'https://du-gae-books-dot-nyt-du-prd.appspot.com/redirect?url1=https%3A%2F%2Fwww.indiebound.org%2Fbook%2F9781250144058%3Faff%3DNYT&url2=https%3A%2F%2Fwww.indiebound.org%2Fsearch%2Fbook%3Fkeys%3DWISH%2BBarbara%2BO%2527Connor%26aff%3DNYT',
-      },
-    ],
-    __v: 0,
-  },
-];
-
-localStorage.setItem('books', JSON.stringify(data));
-
-///////////////////////
-const buttonjs = document.querySelector('.js-button');
-const addElem = document.querySelector('.js-markup');
-
-buttonjs.addEventListener('click', storageData);
-
+const addElem = document.querySelector('.shopping__books-list');
+console.log(addElem)
 let massive = [];
 let idBooks = [];
 let parsedBook;
 
+storageData()
+
 function storageData() {
-  const book = localStorage.getItem('books');
+  const book = localStorage.getItem('arrayStorage');
 
   parsedBook = JSON.parse(book);
   parsedBook.map(arr => {
@@ -159,8 +34,7 @@ function storageData() {
 function createMarkup(arr) {
   let markup = '';
   arr.map(({ _id, book_image, title, publisher, description, author }) => {
-    markup += `<ul class="shopping__books-list" id="${_id}">
-<li class="books-list__item">
+    markup += `<li class="books-list__item" id="${_id}">
   <img class="books-list__image" src="${book_image}" alt="${title}">
   <div class="books-info">
   <button type="button" class="dump-button" >
@@ -179,8 +53,7 @@ function createMarkup(arr) {
     <li class="book-shop"><a href="https://books-shop.com.ua/"><img src="${bookshopPNG}" alt="Book Shop"></a></li>
   </ul>
     </div>
-    </li>   
-</ul>`;
+    </li>`;
   });
 
   addElem.insertAdjacentHTML('afterbegin', markup);
